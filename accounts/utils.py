8d6dde78e6e0d92eec5ab934f750cfa5121b0539
BokijonovS <sanatjon.dark@gmail.com>
2024-07-17 16:56:51 +0500

    updated

--- accounts/utils.py
+++ accounts/utils.py
@@ -2,7 +2,7 @@ def percentage_counter(int1, int2):
     a = int2 - int1
     b = int2 / 100
     c = a / b
-    c = round(c, 2)
+    c = round(c, 1)
     if c == 0:
         return f"{c}"
-    return f'{c}%'
+    return f'{-c}%'
