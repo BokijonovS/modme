c6be67316cb966cc71b0871226cef6bc3454cc3b
BokijonovS <sanatjon.dark@gmail.com>
2024-07-18 13:42:58 +0500

    leads models added

--- leads/admin.py
+++ leads/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
+from leads.models import Lead, Expectation, Set
+
 # Register your models here.
+
+admin.site.register(Lead)
+admin.site.register(Expectation)
+admin.site.register(Set)
+
--- leads/models.py
+++ leads/models.py
@@ -1,10 +1,53 @@
 from django.db import models
 
 from accounts.models import User
+from courses.models import Group
 
 
 # Create your models here.
 
+class Source(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
+class Location(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
 
 class Lead(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
+    source = models.ForeignKey(Source, on_delete=models.CASCADE)
+    location = models.ForeignKey(Location, on_delete=models.CASCADE)
+    phone = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.user.first_name
+
+
+class Expectation(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    source = models.ForeignKey(Source, on_delete=models.CASCADE)
+    location = models.ForeignKey(Location, on_delete=models.CASCADE)
+    phone = models.CharField(max_length=100)
+    test_date = models.DateField()
+
+    def __str__(self):
+        return self.user.first_name
+
+
+class Set(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    source = models.ForeignKey(Source, on_delete=models.CASCADE)
+    location = models.ForeignKey(Location, on_delete=models.CASCADE)
+    phone = models.CharField(max_length=100)
+    test_date = models.DateField()
+    group = models.ForeignKey(Group, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.user.first_name
