f36c2a1ec74ad107c6358e5b07b3995b06a0b909
BokijonovS <sanatjon.dark@gmail.com>
2024-07-18 14:16:32 +0500

    lead default apis added

--- leads/urls.py
+++ leads/urls.py
@@ -1,9 +1,13 @@
 from django.urls import path, include
+from .views import *
+from rest_framework import routers
 
-# from rest_framework import routers
-#
-# router = routers.DefaultRouter()
+
+router = routers.DefaultRouter()
+router.register('leads', LeadViewSet)
+router.register('expectations', ExpectationViewSet)
+router.register('sets', SetViewSet)
 
 urlpatterns = [
-    # path('', include('router.urls'))
+    path('', include(router.urls))
 ]
--- leads/views.py
+++ leads/views.py
@@ -1,4 +1,24 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+
+from .models import Lead, Expectation, Set
+from .serializers import *
+
 
 # Create your views here.
+
+
+class LeadViewSet(viewsets.ModelViewSet):
+    queryset = Lead.objects.all()
+    serializer_class = LeadSerializer
+
+
+class ExpectationViewSet(viewsets.ModelViewSet):
+    queryset = Expectation.objects.all()
+    serializer_class = ExpectationSerializer
+
+
+class SetViewSet(viewsets.ModelViewSet):
+    queryset = Set.objects.all()
+    serializer_class = SetSerializer
 
